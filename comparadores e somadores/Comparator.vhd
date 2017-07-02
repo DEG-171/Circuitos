cb740648936dbd2d4a72c8bac6631232bd2c09a5
DEG-171 <deg.cco.171@gmail.com>
2017-07-02 19:10:36 -0300

    Update Comparator.vhd

--- comparadores e somadores/Comparator.vhd
+++ comparadores e somadores/Comparator.vhd
@@ -1,5 +1,20 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
 
-entity comparator is 
-  
+entity Comparator is
+generic (N : natural := 64);
+port( 
+		CNT_X, B: in std_logic_vector((N-1) downto 0);
+		resul: out std_logic
+		);
+end Comparator;
+
+architecture comp of Comparator is
+	begin
+		if CNT_X = B then
+			resul = '1';
+		else
+			resul = '0';
+		end if;
+end comp;
